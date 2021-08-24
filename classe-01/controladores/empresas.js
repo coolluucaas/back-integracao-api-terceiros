77830f57924a8784b88f09665baf2b4ef4f2f10d
coolluucaas <lucaspsanta@gmail.com>
2021-08-24 18:25:38 -0300

    configuração do instância do axios

--- classe-01/controladores/empresas.js
+++ classe-01/controladores/empresas.js
@@ -1,4 +1,13 @@
-const obterInfoEmpresa = async (req, res) => {}
+const instanciaAxios = require('../servicos/company_enrichment')
+
+const obterInfoEmpresa = async (req, res) => {
+    try {
+        const infoPromise
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 module.exports = {
     obterInfoEmpresa
