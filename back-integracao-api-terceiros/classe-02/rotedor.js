d7b04e83823af7b6abc4792f2854c657f268dbcb
coolluucaas <lucaspsanta@gmail.com>
2021-09-02 13:44:07 -0300

    roteador confgurado

---
+++ back-integracao-api-terceiros/classe-02/rotedor.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const { verificarLocalizacao } = require('./controladores/votos')
+const roteador = express()
+
+roteador.post('/votacao/:pais/:ip', verificarLocalizacao )
+
+module.exports = {
+    roteador
+}
