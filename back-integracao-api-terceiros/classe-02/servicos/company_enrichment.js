01f55216fd7003070858eeca156bc4cb144bc36a
coolluucaas <lucaspsanta@gmail.com>
2021-09-02 13:58:55 -0300

    configuração da instancia do axios

---
+++ back-integracao-api-terceiros/classe-02/servicos/company_enrichment.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+
+const instanciaAxios = axios.create({
+    baseURL: 'https://ipgeolocation.abstractapi.com/v1/',
+    params: {
+        api_key = '9b21541f677c4baeb53e0bec2bc13c05'
+    }
+})
+
+
